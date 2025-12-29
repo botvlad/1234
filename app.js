009a41436ec09b541523dfa9d27149dd1a0a4846
botvlad <g11410900@gmail.com>
2025-12-29 19:47:34 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
-const synth=speechSynthesis;
-const voiceSel=document.getElementById('voice');
-
-function load(){
- const v=synth.getVoices().filter(x=>x.lang.startsWith('es'));
- voiceSel.innerHTML='';
- v.forEach((x,i)=>{
-  const o=document.createElement('option');
-  o.value=i;o.textContent=x.name;
-  voiceSel.appendChild(o);
- });
+function play(){
+ const a=document.getElementById('a');
+ a.src='voice1.wav';
+ a.play();
 }
-synth.onvoiceschanged=load;load();
-
-document.getElementById('speak').onclick=()=>{
- const t=document.getElementById('text').value;
- if(!t)return;
- const u=new SpeechSynthesisUtterance(t);
- u.voice=synth.getVoices().filter(x=>x.lang.startsWith('es'))[voiceSel.value];
- synth.speak(u);
-};
